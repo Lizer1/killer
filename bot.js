fdb670cf6a7fff62a7f8f52284b5962564915128
YOYOMOSHKELA <43928378+YOYOMOSHKELA@users.noreply.github.com>
2019-01-30 15:34:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -816,7 +816,24 @@ message.channel.sendEmbed(embed);
 
 
 
+client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
 
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 10,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+  message.channel.send("**تم ارسال الرابط برسالة خاصة**")
+
+message.author.send(`**https://discord.gg/MemWmU مدة الرابط : يـوم
+عدد استخدامات الرابط : 5**`)
+
+
+    }
+});
 
 
 
