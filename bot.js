0f656360cfeebfb8474d3384079b45335994f2d0
YOYOMOSHKELA <43928378+YOYOMOSHKELA@users.noreply.github.com>
2019-01-30 15:42:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -838,51 +838,6 @@ message.author.send(`**https://discord.gg/MemWmU مدة الرابط : يـوم
 
 
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const adminprefix = "#";
-const developers = ['453018895780151306'];
-
-console.log("BaRooooooD");
-
-client.on('ready', () => {
-    console.log(`Logged as ${client.user.tag}`)
-})
-
-
-client.on('message', message => { 
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**تم تغيير البلاينق الى   ${argresult}**`)
-  } else 
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**تَم تغيير الواتشينق الى   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**تَم تغيير الليسينينق الى   ${argresult}**`)
-  } else
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/paaaaaartly");
-      message.channel.send(`تم تغييرك حالتك بالتويتش الى   **${argresult}**`)
-  }
-  if (message.content.startsWith(adminprefix + 'sn')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`جاري تغيير الأسم لـ ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'sa')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`**جاري تغيير الأفتار... :** `);
-}
-});
-
 
 
 
